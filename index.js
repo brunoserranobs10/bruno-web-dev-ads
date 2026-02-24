8f88643e265526522e50fac7c006283d982b14df
unilago <unilago@unilago.edu.br>
2026-02-24 20:36:11 -0300

    COMMIT INICIAL

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express()
+
+app.use("/", function (req, res){
+    res.send("<h1>funcionou</h1")
+})
+app.listen(3000, ()=> {
+    console.log('servidor rodando em http://localhost:3000')
+})
